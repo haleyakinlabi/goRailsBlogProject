6baf4f26be08eb6bf24bbeb2c7def2a9bbfea752
Haley Akinlabi (haleyakinlabi) <haley.akinlabi@clickfunnels.com>
2023-08-08 09:32:30 -0600

    save

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ gem "jbuilder"
 # gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
@@ -46,11 +46,11 @@ gem "bootsnap", require: false
 # gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+gem "image_processing", "~> 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "debug", platforms: %i[mri mingw x64_mingw]
 end
 
 group :development do
@@ -62,6 +62,7 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  gem "standard"
 end
 
 group :test do
@@ -74,3 +75,5 @@ end
 gem "devise", "~> 4.9"
 
 gem "tailwindcss-rails", "~> 2.0"
+
+gem "pagy", "~> 6.0"
--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,51 +2,52 @@ class BlogPostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_blog_post, except: [:index, :new, :create]
 
-  def index 
-    @blog_posts = BlogPost.all
-  end 
+  def index
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
+    @pagy, @blog_posts = pagy(@blog_posts)
+  end
 
-  def show 
-  end 
+  def show
+  end
 
-  def new 
-    @blog_post = BlogPost.new 
-  end 
+  def new
+    @blog_post = BlogPost.new
+  end
 
-  def create 
+  def create
     @blog_post = BlogPost.new(blog_post_params)
     if @blog_post.save
       redirect_to @blog_post
-    else 
-      render :new, status: :unprocessable_entity 
-    end 
-  end 
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-  def edit 
-  end 
+  def edit
+  end
 
-  def update 
+  def update
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
-    else 
+    else
       render :edit, status: :unprocessable_entity
-    end 
-  end 
+    end
+  end
 
   def destroy
     @blog_post.destroy
     redirect_to root_path
-  end 
+  end
 
-  private 
+  private
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
-  end 
+    params.require(:blog_post).permit(:title, :content, :published_at)
+  end
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    redirect_to root_path 
-  end 
-end 
+    redirect_to root_path
+  end
+end
